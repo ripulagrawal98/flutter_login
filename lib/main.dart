d08356cd49dfa2061486bf907def724a6b829694
Ripul Agrawal <ripul98@outlook.com>
2020-07-22 22:39:55 +0530

    dashboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -137,23 +138,123 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-//        appBar: AppBar(
-//          title: Text("Second Screen"),
-//        ),
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          title: Text("First Screen"),
+        ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-
-              RaisedButton(
-                onPressed: () {
-//              Navigator.pop(context);
-                  Navigator.pushNamed(context, '/second');
-                  // Navigate back to first screen when tapped.
-                },
-                child: Text('Go back!'),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 20.0,horizontal: 100.0),
+                color: Colors.lightBlue,
+                child: Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Icon(
+                        Icons.local_hospital,
+                        size:25.0,
+                      ),
+                      Text(
+                        "Gardimeter",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 30.0,
+                          fontFamily: "Lobster",
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(200.0),
+                ),
               ),
+              Card(
+                color: Colors.redAccent,
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Agrawal's Clinic",
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Active_Patients / Max_patients",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Waiting List",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "SourceSansPro",
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white70,
+                      ),
+                    )
+                  ],
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {
+                    //Navigator.pop(context);
+                      Navigator.pushNamed(context, '/second');
+                      // Navigate back to first screen when tapped.
+                    },
+                    child: Text('Take Token'),
+                    color: Colors.yellow,
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+//              Navigator.pop(context);
+                      Navigator.pushNamed(context, '/second');
+                      // Navigate back to first screen when tapped.
+                    },
+                    child: Text('Book Appointment'),
+                    color: Colors.yellow,
+                  ),
+                ],
+              )
+
+
 
             ],
 
