218072ec8d75935b8427285a6fe974d166169c7b
Ripul Agrawal <ripul98@outlook.com>
2020-07-22 21:27:17 +0530

    navigation b/w screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      // When navigating to the "/" route, build the FirstScreen widget.
+      '/': (context) => SecondScreen(),
+      // When navigating to the "/second" route, build the SecondScreen widget.
+      '/second': (context) => FirstScreen(),
+    },
+
+  ));
 }
-class MyApp extends StatelessWidget {
+
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+     debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text("Login Page"),
         ),
-      body: SafeArea(
+        body: SafeArea(
 
         child: Column(
           children: <Widget>[
@@ -79,7 +89,10 @@ class MyApp extends StatelessWidget {
               ),
             ),
             FlatButton(
-              onPressed: (){ },
+              onPressed: (){
+                Navigator.pop(context);
+//                Navigator.pushNamed(context, '/second');
+              },
               color: Colors.white,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0)
@@ -94,7 +107,10 @@ class MyApp extends StatelessWidget {
               ),
             ),
             FlatButton(
-              onPressed: (){ },
+              onPressed: (){
+                Navigator.pop(context);
+//              Navigator.pushNamed(context, '/second');
+              },
               color: Colors.white,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)
@@ -116,4 +132,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+//        appBar: AppBar(
+//          title: Text("Second Screen"),
+//        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
 
+              RaisedButton(
+                onPressed: () {
+//              Navigator.pop(context);
+                  Navigator.pushNamed(context, '/second');
+                  // Navigate back to first screen when tapped.
+                },
+                child: Text('Go back!'),
+              ),
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
